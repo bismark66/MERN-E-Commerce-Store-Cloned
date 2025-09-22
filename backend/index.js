9942a2039d9ef87dd461ba6d342766083d6b932e
Bismark <agyarebismark6@gmail.com>
2025-09-22 12:18:26 +0000

    Update server configuration: set port to 6000 and enable CORS

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -14,7 +15,7 @@ import webhookRoutes from "./routes/webhookRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 connectDB();
 
@@ -23,6 +24,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors("*"));
+
+// app.use(ImageKit);
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
@@ -30,6 +34,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/webhooks", webhookRoutes);
 app.use("/api/orders", orderRoutes);
+
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello world!" });
