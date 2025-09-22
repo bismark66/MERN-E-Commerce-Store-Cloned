ca8d1294b7c109da41e011c9a7e0157f60e1f6e2
Bismark <agyarebismark6@gmail.com>
2025-09-22 12:19:28 +0000

    Refactor loginUser to return access token and restructure response; add createAdminUser function for admin user creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,16 +48,52 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 
     if (isPasswordValid) {
-      createToken(res, existingUser._id);
+      const token = createToken(res, existingUser._id);
 
       res.status(201).json({
-        _id: existingUser._id,
-        username: existingUser.username,
-        email: existingUser.email,
-        isAdmin: existingUser.isAdmin,
+        user: {
+          _id: existingUser._id,
+          username: existingUser.username,
+          email: existingUser.email,
+          isAdmin: existingUser.isAdmin,
+        },
+        accessToken: token,
       });
       return;
     }
+  }
+});
+
+// create admin user
+const createAdminUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the inputs.");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("User already exists");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+    isAdmin: true,
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
 });
 
@@ -71,7 +107,9 @@ const logoutCurrentUser = asyncHandler(async (req, res) => {
 });
 
 const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({
+    isAdmin: false,
+  });
   res.json(users);
 });
 
@@ -177,4 +215,5 @@ export {
   deleteUserById,
   getUserById,
   updateUserById,
+  createAdminUser,
 };
